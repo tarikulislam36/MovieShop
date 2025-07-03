bd2ac67cb7b1452896f2d75d47525edacf789b07
tarikulislam <tarikulislam4145@gmail.com>
2025-07-03 05:37:25 +0530

    Extend TailWind style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   content: ["app/_layout.tsx", "./components/**/*.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1f2937", // Gray 900
+        secondary: "#f87171", // Red 400
+        accent: "#3b82f6", // Blue 500
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+    },
   },
   plugins: [],
 }
