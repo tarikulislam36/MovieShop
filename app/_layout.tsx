ef1dd195cc68bf732eb1f7230d8606bcd497f919
tarikulislam <tarikulislam4145@gmail.com>
2025-07-11 04:01:24 +0530

    navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,19 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import "./global.css"
-
+import { Stack } from 'expo-router'
 
 const _layout = () => {
   return (
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={
+          { headerShown: false }
+        }
 
-    <SafeAreaView className="flex-1">
-      <View className="flex-1 items-center justify-center bg-red-900">
-        <Text className="text-xl font-bold text-red-500">
-          Welcome to Nativewind!
-        </Text>
-      </View>
-    </SafeAreaView>
+      />
 
+    </Stack>
   )
 }
 
