6dc11f72ea60d46afc2704e76067c201cee104e0
tarikulislam <tarikulislam4145@gmail.com>
2025-07-16 21:19:15 +0530

    TabBar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,31 +7,70 @@ import { icons } from '@/constants/icons'
 
 // TabBar Icon component
 const TabIcon = ({ focused, icon, title }) => {
-    return (
-        <ImageBackground
-            source={images.highlight}
-            className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full
+    if (focused) {
+        return (
+            <ImageBackground
+                source={images.highlight}
+                className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4  justify-center items-center rounded-full
                                      overflow-hidden"
-        >
+            >
+                <Image
+                    source={icon}
+                    tintColor="#151312"
+                    className="w-6 h-6"
+
+
+                />
+                <Text>  {title}</Text>
+
+
+
+            </ImageBackground>
+
+        )
+    }
+    return (
+        <View className="size-full justify-center items-center mt-4 rounded-full">
             <Image
                 source={icon}
-                tintColor="#151312"
-                className="w-6 h-6"
-
+                tintColor="#A8B5BD"
+                className="size-5"
 
             />
-            <Text>  Home</Text>
-
-
-
-        </ImageBackground>
-
+            {/* <Text>  {title}</Text> */}
+        </View>
     )
 }
 
 const _layout = () => {
     return (
-        <Tabs>
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarItemStyle: {
+                    width: "100%",
+                    height: "100%",
+                    justifyContent: "center",
+                    alignItems: "center"
+
+                },
+                tabBarStyle: {
+                    backgroundColor: '#0f0D23',
+                    position: 'absolute',
+                    borderRadius: 50,
+                    marginHorizontal: 20,
+                    marginBottom: 36,
+                    height: 52,
+                    overflow: 'hidden',
+                    borderWidth: 1,
+                    borderColor: '#0f0D23',
+
+                }
+
+
+            }}
+
+        >
             <Tabs.Screen
 
                 name="index"
