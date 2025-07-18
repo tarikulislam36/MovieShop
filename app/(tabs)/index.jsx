c433472262115688aafcc2e6052c10ffec615819
tarikulislam <tarikulislam4145@gmail.com>
2025-07-18 23:45:25 +0530

    home

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,10 +1,20 @@
-import { View, Text } from 'react-native'
+import { Image, View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import { images } from '@/constants/images'
+import { icons } from '@/constants/icons'
+
+
 
 const index = () => {
     return (
-        <View>
-            <Text>index</Text>
+        <View
+            className='flex-1 bg-primary'
+        >
+            <Image source={images.bg} className='absolute w-full z-0' />
+            <ScrollView className='flex-1 px-5 ' showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: '100%', paddingBottom: 10 }}>
+                <Image source={icons.logo} className='w-12 h-10 mt-20 mb-5 mx-auto' />
+            </ScrollView>
+
         </View>
     )
 }
